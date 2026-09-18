c1d8c3c181c4c70d4d28de9308f84b2c71d84616
MuriloSilvaCoelho <murilocoelho0706@gmail.com>
2026-09-18 00:30:43 -0300

    Delete src directory

--- src/main/java/com/company/abrirchamado/Prioridade.java
+++
@@ -1,8 +0,0 @@
-package com.company.abrirchamado;
-
-public enum Prioridade {
-
-    BAIXA,
-    MEDIA,
-    ALTA
-}
--- src/main/java/com/company/abrirchamado/StatusChamado.java
+++
@@ -1,8 +0,0 @@
-package com.company.abrirchamado;
-public enum StatusChamado {
-
-    ABERTO,
-    EM_ATENDIMENTO,
-    AGUARDANDO_CLIENTE,
-    ENCERRADO
-}
